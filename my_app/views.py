dedae9a2fd43e97d30e43d8a3754e41835f23a9e
shait123 <shaitansighr214@gmail.com>
2024-11-20 00:12:24 +0530

    Your commit message here

--- my_app/views.py
+++ my_app/views.py
@@ -4,8 +4,4 @@ def example_view(request):
     return render(request,'my_app/example.html')
 
 def variable_view(request):
-    my_var= {
-        'first_name':'Shaitan','last_name':'Singh',
-        'some_list':[1,2,3],'some_dic':{'some_key':'some_value'}
-    }
-    return render(request,'my_app/variable.html',context=my_var)
+    return render(request,'my_app/variable.html')
